58d426813bf0cae0a83515f22a049023c764296a
ecesuurker <ecesu.urker@ug.bilkent.edu.tr>
2021-04-18 23:56:56 +0300

    Added two different experiment instruction files for two different experiments. Also added run function to python script.

--- PsychopyExercise.py
+++ PsychopyExercise.py
@@ -6,8 +6,11 @@ class Experiment():
     def __init__(self, ParticipantNumber, Category):
         self.PNum = ParticipantNumber
         self.category = Category
-        
- 
+        self.trialType = random.randint(1, 2)
+        fileName = ParticipantNumber + "_" + Category + "TT" + str(self.trialType) + ".csv"
+        self.data = open(fileName, "w")
+        trialFile = "TrialType" + str(self.trialType) + ".txt"
+        self.trialFileName = open(trialFile, "r", newline=None)
     
     def Instructions(self, fileName):
         inst = open(fileName, "r") #Instructions are read from Instructions.txt file
@@ -22,16 +25,39 @@ class Experiment():
         expWin.flip()
         event.waitKeys(keyList=["space"]) #The stimuli on the window only change when space key is pressed
         
-    def Stimuli(self):
-        stimulus1 = visual.Rect(expWin, width=200, height=100, lineWidth=2, lineColor="black", fillColor="red", pos=(-300,0))
-        stimulus1.draw()
-        stimulus2 = visual.Rect(expWin, width=200, height=100, lineWidth =2, lineColor="black", fillColor="black", pos=(300,0))
-        stimulus2.draw()
-        expWin.flip()
-        event.waitKeys(keyList=["space"])
+    def Run(self):
+        trials = csv.DictReader(self.trialFileName, delimiter="\t") #Reads the trial structure, the sizes of rectangles etc.
+        dataFile = csv.DictWriter(self.data, fieldnames=["RT","KeyPress"]) #This is the data file that the participant data will be written
+        dataFile.writeheader()
+        for trial in trials:
+            for i in range(int(trial["TrialNumber"])): #Determines how many trials each block have
+                sizes = [int(trial["WidthofStimulus1"]), int(trial["WidthofStimulus2"])]
+                random.shuffle(sizes) #This is for randomizing the presentation side of each stimulus
+                timeFrame = core.StaticPeriod(win=expWin) #To determine time period for each trial
+                timeFrame.start(0.5)
+                stimulus1 = visual.Rect(expWin, height=100, width=sizes[0], lineWidth=2, lineColor="black", fillColor="red", pos=(-300,0))
+                stimulus1.draw() #Stimuli presentations
+                stimulus2 = visual.Rect(expWin, height=100, width=sizes[1], lineWidth =2, lineColor="black", fillColor="black", pos=(300,0))
+                stimulus2.draw()
+                if "," in trial["Names"]: #If two stimuli that belong to different categories are presented both of their category name is presented
+                    name1 = trial["Names"][1:trial["Names"].find(",")] #Extracting names of categories from the file
+                    name2 = trial["Names"][trial["Names"].find(",")+1:-1]
+                    if sizes[0] != int(trial["WidthofStimulus1"]): 
+                        name1, name2 = name2, name1 #Determines whether shuffling change the order, if it did it changes category names as well
+                    
+                    disp1 = visual.TextStim(expWin, color=txtColor, text=name1, pos=(-300,-100))
+                    disp2 = visual.TextStim(expWin, color=txtColor, text=name2, pos= (300, -100))
+                    disp1.draw() #Category names are displayed
+                    disp2.draw()
 
-            
-        
+                        
+                else:
+                    disp = visual.TextStim(expWin, color=txtColor, text=trial["Names"], pos=(0,-100))
+                    disp.draw() 
+                        
+                expWin.flip()
+                timeFrame.complete()
+                
 
 bgColor = "grey" #First I determined the background color and text color
 txtColor = "black"       
@@ -46,7 +72,7 @@ if not expDlg.OK: #If the participant does not click OK the window closes
     
 exp = Experiment(PData[0], PData[1])
 exp.Instructions("Instructions.txt")
-exp.Stimuli()
+exp.Run()
     
 
     
